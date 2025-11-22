2ba2fa491c4e218f025eb76bd304bbc78d36d762
11249a129-code <11249a129@kanchiuniv.ac.in>
2025-11-22 10:44:19 +0530

    Create Doubly linked list.c

---
+++ Doubly linked list.c
@@ -0,0 +1,51 @@
+// Main function
+int main()
+{
+    struct Node *head = NULL;
+    int choice, data, pos;
+
+    while (1)
+    {
+        printf("\n--- Linked List Menu ---\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert at Position\n");
+        printf("4. Delete by Value\n");
+        printf("5. Display List\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            InsertAtBeginning(&head, data);
+            break;
+        case 2:
+            printf("Enter data to insert: ");
+            scanf("%d", &data);
+            InsertAtEnd(&head, data);
+            break;
+        case 3:
+            printf("Enter data and position to insert: ");
+            scanf("%d %d", &data, &pos);
+            InsertAtPosition(&head, data, pos);
+            break;
+        case 4:
+            printf("Enter value to delete: ");
+            scanf("%d", &data);
+            DeleteNode(&head, data);
+            break;
+        case 5:
+            DisplayList(head);
+            break;
+        case 6:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice! Try again.\n");
+        }
+    }
+    return 0;
